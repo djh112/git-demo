db6bae28d02760b318ed4069e46822d82a6d4ce9
djh <3527548711@qq.com>
2025-01-07 13:13:09 +0800

    演示版本二

--- src/main/java/com/powernode/git/service/impl/CatServiceImpl.java
+++ src/main/java/com/powernode/git/service/impl/CatServiceImpl.java
@@ -3,5 +3,6 @@ package com.powernode.git.service.impl;
 public class CatServiceImpl {
     public static void main(String[] args) {
         System.out.println("hello git!版本一");
+        System.out.println("hello git!版本二");
     }
 }
